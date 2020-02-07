9f8ed89e821d2aae115f460376a159e093dcbf20
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-02-07 11:48:26 -0200

    servidor teste funcionado

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const alunos = [
+  {
+    nome: "Aluno1",
+    idade: 12
+  },
+  {
+    nome: "Aluno2",
+    idade: 13
+  },
+  {
+    nome: "Aluno3",
+    idade: 14
+  },
+  {
+    nome: "Aluno4",
+    idade: 15
+  }
+];
+
+app.get("/", (req, res) => {
+  res.send(alunos);
+});
+
+app.listen(3000);
